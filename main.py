12a293ea94683cd0e71d00b946bf09247a14c113
faikarda <112086875+faikarda@users.noreply.github.com>
2025-01-15 11:45:39 +0000

    create store

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ class OrderConfirmedEvent(Event):
 class OrderRejectedEvent(Event):
     def __init__(self, customer_name, product_name, reason):
         super().__init__("order_rejected", {"customer_name": customer_name, "product_name": product1 , "reason": wrong })
+
+class Store:
+    def __init__(self, name, inventory, event_queue):
+        self.name = name
+        self.inventory = inventory
+        self.event_queue = event_queue
