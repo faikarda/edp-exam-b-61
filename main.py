0a813678de64521894b0d93aecb544682318d5c1
faikarda <112086875+faikarda@users.noreply.github.com>
2025-01-15 11:43:11 +0000

    order rejection

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ class OrderSubmittedEvent(Event):
 class OrderConfirmedEvent(Event):
     def __init__(self, customer_name, product_name):
         super().__init__("order_confirmed", {"customer_name": Arda  , "product_name": product1 })
+
+class OrderRejectedEvent(Event):
+    def __init__(self, customer_name, product_name, reason):
+        super().__init__("order_rejected", {"customer_name": customer_name, "product_name": product1 , "reason": wrong })
