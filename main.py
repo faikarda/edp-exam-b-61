3346bec658314ba85df8909540ffdf9f4b7a8c5a
faikarda <112086875+faikarda@users.noreply.github.com>
2025-01-15 11:41:09 +0000

    order confirmation

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ class Event:
 
 class OrderSubmittedEvent(Event):
      def __init__(self, customer_name, product_name):
-        super().__init__("order_submitted", {"customer_name": customer_name, "product_name": product_name})
+        super().__init__("order_submitted", {"customer_name": Arda  , "product_name":product1 })
+
+class OrderConfirmedEvent(Event):
+    def __init__(self, customer_name, product_name):
+        super().__init__("order_confirmed", {"customer_name": Arda  , "product_name": product1 })
